3473269d9bd40018ea802bd3c85e40dbbea39c3b
skimehorn <skimehornwork@gmail.com>
2023-08-13 13:04:14 -0500

    Final Product Finished

---
+++ src/components/Banner.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styling.css"
+export default function Banner() {
+    return(
+        <div>
+        <h1>Orbit Report</h1>
+        <p>Click a satallite to see its orbit type!</p>
+        </div>
+    );
+}
